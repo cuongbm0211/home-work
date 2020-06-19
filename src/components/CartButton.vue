44f24daa40d5a2dec042550120e94aa5671722a6
cuongbm <cuong.bui@niteco.se>
2020-06-19 12:13:55 +0700

    Add UI CartButton

---
+++ src/components/CartButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card-text style="height: 100px; position: relative">
+    <v-fab-transition>
+      <v-btn v-show="!hidden" color="black" dark absolute top right fab>
+        <v-icon>mdi-cart</v-icon>
+      </v-btn>
+    </v-fab-transition>
+  </v-card-text>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    hidden: false
+  })
+};
+</script>
--- src/layouts/baseline.vue
+++ src/layouts/baseline.vue
@@ -26,22 +26,22 @@
       <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
 
-    <v-main>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col class="text-center">
-            <v-tooltip left>
-              <template v-slot:activator="{ on }">
-                <v-btn :href="source" icon large target="_blank" v-on="on">
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col class="text-center">
+          <v-tooltip left>
+            <template v-slot:activator="{ on }">
+              <v-btn :href="source" icon large target="_blank" v-on="on">
+                <v-icon large>mdi-code-tags</v-icon>
+              </v-btn>
+            </template>
+            <span>Source</span>
+          </v-tooltip>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <CartButton />
     <v-footer color="indigo" app>
       <span class="white--text">&copy; 2019</span>
     </v-footer>
@@ -52,6 +52,9 @@
 export default {
   props: {
     source: String
+  },
+  components: {
+    CartButton: () => import("../components/CartButton")
   },
   data: () => ({
     drawer: null
